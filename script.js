6f83a3b605da1ab4b1a68fa76cf87545bcbf9f2c
Ayush <ayushtriescode@gmail.com>
2026-04-19 14:06:22 +0530

    Added projects

---
+++ script.js
@@ -0,0 +1,37 @@
+function createSlider(container) {
+  const slides = container.querySelectorAll(".slide");
+  const nextBtn = container.querySelector(".next");
+  const prevBtn = container.querySelector(".prev");
+
+  let counter = 0;
+
+  slides.forEach((slide, index) => {
+    slide.style.left = `${index * 100}%`;
+  });
+
+  const slideImage = () => {
+    slides.forEach((slide) => {
+      slide.style.transform = `translateX(-${counter * 100}%)`;
+    });
+  };
+
+  nextBtn.onclick = () => {
+    if (counter < slides.length - 1) {
+      counter++;
+      slideImage();
+    }
+  };
+
+  prevBtn.onclick = () => {
+    if (counter > 0) {
+      counter--;
+      slideImage();
+    }
+  };
+}
+
+const slider1 = document.querySelector(".slider1");
+const slider2 = document.querySelector(".slider2");
+
+createSlider(slider1);
+createSlider(slider2);
